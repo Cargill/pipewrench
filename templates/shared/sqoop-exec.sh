edcc3a8dd7d048f82644b6d29af91ab216508614
Tony Foerster <anthonymfoerster@gmail.com>
2017-10-25 07:29:27 -0500

    Change interpreter to use environment (for Travis)

--- templates/shared/sqoop-exec.sh
+++ templates/shared/sqoop-exec.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #    Copyright 2017 Cargill Incorporated
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
--- templates/sqoop-parquet-hdfs-impala/test.sh
+++ templates/sqoop-parquet-hdfs-impala/test.sh
@@ -11,7 +11,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License. -#}
-#!/bin/bash
+#!/usr/bin/env bash
 # Run a simple test to compare table counts
 set -euo pipefail
 ACTUAL=$(<test-kudu-rowcount)
