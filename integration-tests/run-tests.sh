a14cee1880199ea3b95c7fb16fdf243c412bb1bc
Tony Foerster <tfoerster@edge1.valhalla.phdata.io>
2017-11-24 15:20:13 -0600

    Simplify testing logic
    Each test gets its own script. Makes fewer loops and is more customizable.

--- integration-tests/run-tests.sh
+++ integration-tests/run-tests.sh
@@ -14,47 +14,8 @@
 #    limitations under the License.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-function cleanup {
-$SCRIPT_DIR/mysql-docker-container-delete || true
-}
-
 trap cleanup EXIT
 cd $SCRIPT_DIR
-# Simple test that builds all example pipelines
-# and does some Makefile validation
-./mysql-docker-container-create
-
-while :;do
-    mysql -h 127.0.0.1 -P 3306 -u pipewrench -ppipewrench -e 'show databases;' &> /dev/null
-    
-    if [[ "$?" -eq "0" ]];then
-        echo 'service ready'
-        break;
-    fi
-    echo 'waiting for mysql container to be available'
-    sleep 1
-done
-docker ps
-set -euo pipefail
-tests=( "sqoop-parquet-hdfs-kudu-impala" "sqoop-parquet-hdfs-impala" )
-
-for pipeline in "${tests[@]}";do
-    pushd ${pipeline}
-    # verify we can generate scripts without error
-    ./generate-scripts
-    pushd ./output/${pipeline}
-    for table in titanic baseball vocab;do
-       pushd ${table}
-       # Run the test-targets script to ensure all targets are available
-       make clean 
-       make source-create
-       make first-run
-       make test-recordcount
-       make source-table-clean
-       make clean
-       popd
-    done
-    popd
-    popd
-done
-
+set -eu
+sqoop-parquet-hdfs-impala/run.sh
+sqoop-parquet-hdfs-kudu-impala/run.sh
