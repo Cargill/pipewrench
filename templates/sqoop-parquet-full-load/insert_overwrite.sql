1d93d177f0e9a4840090d572ce12fafb0fdb0b47
Kiran Kumar Guntupalli <kguntupalli@phdata.io>
2018-01-30 10:06:22 -0600

    Ticks in insert overwrite

--- templates/sqoop-parquet-full-load/insert_overwrite.sql
+++ templates/sqoop-parquet-full-load/insert_overwrite.sql
@@ -1,8 +1,8 @@
 INSERT OVERWRITE TABLE {{ conf.result_database.name }}.{{ table.destination.name }}_parquet PARTITION (mod_val=${var:val})
 SELECT {% for column in table.columns %}
 {%- if column["datatype"].lower() == "decimal" %}
-cast (`{{ column.name }}` as decimal({{column.precision}}, {{column.scale}}) )
-{%- else %} `{{ column.name }}`
+cast (`{{ column.name.replace('/','_') }}` as decimal({{column.precision}}, {{column.scale}}) )
+{%- else %} `{{ column.name.replace('/','_') }}`
 {% endif %}
 {%- if not loop.last -%}, {% endif %}
 {%- endfor %}
