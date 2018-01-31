1a4a88ecdaa88f4f9fe1c017f403fbd18bb158f2
Kiran Kumar Guntupalli <kguntupalli@phdata.io>
2018-01-31 11:02:50 -0600

    Drop parquet

--- templates/sqoop-parquet-full-load/compute-stats.sh
+++ templates/sqoop-parquet-full-load/compute-stats.sh
@@ -5,5 +5,5 @@ val=$((DAY%4))
 impala="{{ conf.impala_cmd }}"
 
 ${impala} compute-stats_avro.sql --var=val=$val
-${impala} compute-stats_result.sql --var=val=$val
+#${impala} compute-stats_result.sql --var=val=$val
 ${impala} compute-stats_final.sql --var=val=$val
