42e7a8d5575bec52bdfb3c1dfb2bb6e37f2d1b15
Richa Meherwal <r138003@drona-edgenode2.cargill.com>
2018-03-18 16:34:49 +0000

    Corrected kudu-table-create template, missing bracket

--- templates/shared/kudu-table-create.sql
+++ templates/shared/kudu-table-create.sql
@@ -16,8 +16,8 @@
 USE {{ conf.staging_database.name }};
 CREATE TABLE IF NOT EXISTS {{ table.destination.name }}_kudu
 {%- set ordered_columns = order_columns(table.primary_keys,table.columns) -%}
-{%- for column in ordered_columns %}
-        {{ column.name }} {{ map_datatypes(column).kudu }}
+({%- for column in ordered_columns %}
+        {{ column.name }}
 {%- if not loop.last -%},{% endif %}
 {%- endfor %},
 primary key ({{ table.primary_keys|join(', ') }}))
