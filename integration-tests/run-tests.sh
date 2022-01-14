62ee541747e30ebce11f9d21b4a98b9edd574bac
aaverbec <43386278+aaverbec@users.noreply.github.com>
2022-01-14 12:47:25 -0600

    Update run-tests.sh

--- integration-tests/run-tests.sh
+++ integration-tests/run-tests.sh
@@ -25,7 +25,8 @@ cd $SCRIPT_DIR
 docker-compose -f $SCRIPT_DIR/docker-compose.yml up -d
 sleep 3
 while :;do
-    docker-compose exec mysql mysql -h localhost -P 3306 -u root -ppipewrench -e 'show databases;' &> /dev/null
+    #docker-compose exec mysql mysql -h localhost -P 3306 -u root -ppipewrench -e 'show databases;' &> /dev/null
+    docker-compose exec mysql mysql -h localhost -P 3306 -u root -ppipewrench -e 'show databases;'
     
     if [[ "$?" -eq "0" ]];then
         echo 'service ready'
