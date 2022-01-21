baaab15bfab234f2392e89134ab6a7f698ff134b
aaverbec <43386278+aaverbec@users.noreply.github.com>
2022-01-21 13:19:36 -0600

    Update run.sh

--- integration-tests/sqoop-parquet-hdfs-kudu-impala/run.sh
+++ integration-tests/sqoop-parquet-hdfs-kudu-impala/run.sh
@@ -3,6 +3,6 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 ./generate-scripts
-docker-compose exec kimpala bash -c 'chown -R hdfs:hdfs /mount'
-docker-compose exec kimpala bash -c 'chmod -R 777 /root'
-docker-compose exec kimpala /mount/sqoop-parquet-hdfs-kudu-impala/run-in-container.sh
+docker exec kimpala bash -c 'chown -R hdfs:hdfs /mount'
+docker exec kimpala bash -c 'chmod -R 777 /root'
+docker exec kimpala /mount/sqoop-parquet-hdfs-kudu-impala/run-in-container.sh
